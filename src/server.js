9332cf7c754c13051835027ce4eab36d93f4b590
rkdghwnd <ghwnd6448@gmail.com>
2024-06-21 14:47:53 +0900

    12

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import { io } from 'socket.io-client';
-const socket = io('https://a9aa-14-36-11-9.ngrok-free.app'); // <-- 백엔드 주소와 연결된 소켓 생성
-socket.setRequestHeader('ngrok-skip-browser-warning', '69420');
+const socket = io('https://a9aa-14-36-11-9.ngrok-free.app', {
+  headers: {
+    'ngrok-skip-browser-warning': '69420',
+  },
+}); // <-- 백엔드 주소와 연결된 소켓 생성
 export default socket;
