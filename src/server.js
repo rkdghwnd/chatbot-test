063d2aac95b6ebbb855d6139aa13629c0b5a91e5
rkdghwnd <ghwnd6448@gmail.com>
2024-06-21 14:27:58 +0900

    10

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
-const socket = io('ff62-14-36-11-9.ngrok-free.app'); // <-- 백엔드 주소와 연결된 소켓 생성
+const socket = io('https://ff62-14-36-11-9.ngrok-free.app'); // <-- 백엔드 주소와 연결된 소켓 생성
 export default socket;
